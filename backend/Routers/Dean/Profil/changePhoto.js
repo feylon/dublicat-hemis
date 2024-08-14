09ecf151c01d7d858e7777d24f4c4329cb3fecef
feylon <jamshid14092002@gmail.com>
2024-08-14 19:53:16 +0500

    Upload js tayyor

---
+++ backend/Routers/Dean/Profil/changePhoto.js
@@ -0,0 +1,77 @@
+import multer from "multer";
+import md5 from "md5";
+import { Router } from "express";
+import { checkToken, get_id } from "../../../functions/jwtDean.js";
+import fs from "fs";
+
+const router = Router();
+const storage = multer.diskStorage({
+    destination : function(req, file, cb){
+        cb(null, 'static/profil_pictures');
+    },
+    filename : function (req, file, cb){
+        let str = file.originalname;
+        let ext = str.split('.')[str.split('.').length - 1]
+        let url = md5(Date.now()) + '.' + ext;
+        cb(null, url);
+        req.body.file = {url, originalname : file.originalname};
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 2 * 1024 * 1024 }, 
+    fileFilter: function (req, file, cb) {
+     
+      const fileTypes = /jpeg|jpg|png|gif/;
+      const mimeType = fileTypes.test(file.mimetype);
+      const extname = fileTypes.test(file.originalname.toLowerCase());
+  
+      if (mimeType && extname) {
+        return cb(null, true);
+      } else {
+        cb(new Error('Only image files are allowed!'));
+      }
+    }
+  });
+
+  router.post("/",  [checkToken, upload.single('image')], async (req, res)=>{
+    const adminId = get_id (req, res);
+
+    try {
+    let fileurl = await global.pool.query("Select profil_url from  dean where id = $1", [adminId]);
+        
+        fs.unlink(`static/${fileurl.rows[0].profil_url}`, (err) => {
+           
+            if (err) {
+                
+            }
+            // console.log('File deleted successfully');
+        });
+} catch (error) {
+        console.log(error);
+        return;
+    }
+
+
+    try {
+        await global.pool.query (`
+            update dean set profil_url = $1 where id = $2;`, ['profil_pictures/' + req.body.file.url, adminId]);
+        return res.status(200).send({Edited : true});
+    } catch (error) {
+        console.log(error)
+    }
+
+  });
+
+
+  router.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+  
+  return res.status(400).send(err.message);
+    } else if (err) {
+    
+      res.status(400).send(err.message);
+    }
+  });
+  export default router;
--- backend/Routers/Dean/index.js
+++ backend/Routers/Dean/index.js
@@ -5,10 +5,12 @@ import login from "./Auth/login.js";
 // Profil
 import getprofil from "./Profil/getprofil.js"
 import changeProfil from "./Profil/changeProfil.js"
-
+import changephotos from "./Profil/changePhoto.js"
+ 
 export default [
 {path : '/login', route : login},
 {path : '/getprofile', route : getprofil},
-{path : '/changeProfile', route : changeProfil}
+{path : '/changeProfile', route : changeProfil},
+{path:"/changephotos", route : changephotos}
 
 ];
